eeec2ed76e2d3cfd8b50ac6084eee4c7366259e3
stormont <stormont@gmail.com>
2015-10-28 00:40:31 -0700

    Updated Main.elm file to match blog post updates.

--- src/Main.elm
+++ src/Main.elm
@@ -16,31 +16,16 @@ type alias Model = List Point        -- A list of points.
 
 -- UPDATE
 
-updateModel : Point -> Model -> Model
-updateModel p model =
+update : Point -> Model -> Model
+update p model =
   let
-    numPoints = 12       -- Number of points to retain.
-    points = p :: model  -- Prepend the latest Point (AKA, "cons").
+    numPoints = 12              -- Number of points to retain.
+    points = p :: model         -- Prepend the latest Point (AKA, "cons").
   in
     List.take numPoints points  -- Update the path (limiting path length).
 
 
 -- VIEW
-
-drawModel : Point -> Model -> Element
-drawModel (w,h) model =
-  collage w h [ drawLine (w,h) model ]  -- Draw the path.
-
-
-drawLine : Point -> List Point -> Form
-drawLine (w,h) points =
-  List.map (\(x,y) -> (toFloat x, toFloat -y)) points  -- Convert the mouse points to
-                                                       -- "model" coordinates.
-    |> path                                      -- Build a path from the points.
-    |> traced lineStyle                          -- Trace the line with defined form.
-    |> move (-(toFloat w) / 2, (toFloat h) / 2)  -- Move drawing from middle to upper
-                                                 -- left ("screen" coordinates).
-
 
 lineStyle : LineStyle
 lineStyle =
@@ -52,18 +37,32 @@ lineStyle =
   }
 
 
+drawLineSegments : (Int,Int) -> List Point -> Form
+drawLineSegments (w,h) points =
+  List.map (\(x,y) -> (toFloat x, toFloat -y)) points  -- Convert the mouse points to
+                                                       -- "model" coordinates.
+    |> path                                      -- Build a path from the points.
+    |> traced lineStyle                          -- Trace the line with defined form.
+    |> move (-(toFloat w) / 2, (toFloat h) / 2)  -- Move drawing from middle to upper
+                                                 -- left ("screen" coordinates).
+
+view : (Int,Int) -> Model -> Element
+view (w,h) model =
+  collage w h [ drawLineSegments (w,h) model ]  -- Draw the path.
+
+
 -- SIGNALS
-
-main =
-  Signal.map2          -- Map two signals together...
-    drawModel          -- ... through the drawModel function.
-    Window.dimensions  -- Use updates to the window dimensions as the first signal.
-    mousePositions     -- Use updates to mouse positions as the second signal.
-
 
 mousePositions : Signal Model
 mousePositions =
   Signal.foldp      -- Fold each signal into an accumulated model...
-    updateModel     -- ... through the updateModel function.
+    update          -- ... through the update function.
     []              -- Start with an empty list.
     Mouse.position  -- Updates given by mouse position changes.
+
+
+main =
+  Signal.map2          -- Map two signals together...
+    view               -- ... through the view function.
+    Window.dimensions  -- Use updates to the window dimensions as the first signal.
+    mousePositions     -- Use updates to mouse positions as the second signal.
